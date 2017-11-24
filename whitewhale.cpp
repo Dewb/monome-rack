78444cb8d0d0b8f0f343c6db3f42ff457334d9cf
Michael Dewberry <michael.dewberry@gmail>
2017-11-24 01:15:48 -0500

    Fix indentation again

--- whitewhale.cpp
+++ whitewhale.cpp
@@ -12,11 +12,11 @@ extern "C" void check_events(void);
 
 // memory state to save
 typedef enum {
-	mTrig, mMap, mSeries
+    mTrig, mMap, mSeries
 } edit_modes;
 
 typedef enum {
-	mForward=0, mReverse, mDrunk, mRandom, mPing, mPingRep
+    mForward=0, mReverse, mDrunk, mRandom, mPing, mPingRep
 } step_modes;
 
 typedef enum {
@@ -25,34 +25,34 @@ typedef enum {
 } ping_direction;
 
 typedef struct {
-	u8 loop_start, loop_end, loop_len, loop_dir;
-	u16 step_choice;
-	u8 cv_mode[2];
-	u8 tr_mode;
-	step_modes step_mode;
+    u8 loop_start, loop_end, loop_len, loop_dir;
+    u16 step_choice;
+    u8 cv_mode[2];
+    u8 tr_mode;
+    step_modes step_mode;
     ping_direction ping_dir;
-	u8 steps[16];
-	u8 step_probs[16];
-	u16 cv_values[16];
-	u16 cv_steps[2][16];
-	u16 cv_curves[2][16];
-	u8 cv_probs[2][16];
+    u8 steps[16];
+    u8 step_probs[16];
+    u16 cv_values[16];
+    u16 cv_steps[2][16];
+    u16 cv_curves[2][16];
+    u8 cv_probs[2][16];
 } whale_pattern;
 
 typedef struct {
-	whale_pattern wp[16];
-	u16 series_list[64];
-	u8 series_start, series_end;
-	u8 tr_mute[4];
-	u8 cv_mute[2];
+    whale_pattern wp[16];
+    u16 series_list[64];
+    u8 series_start, series_end;
+    u8 tr_mute[4];
+    u8 cv_mute[2];
 } whale_set;
 
 typedef const struct {
-	u8 fresh;
-	edit_modes edit_mode;
-	u8 preset_select;
-	u8 glyph[8][8];
-	whale_set w[8];
+    u8 fresh;
+    edit_modes edit_mode;
+    u8 preset_select;
+    u8 glyph[8][8];
+    whale_set w[8];
 } nvram_data_t;
 
 extern whale_set w;
@@ -103,7 +103,7 @@ struct WhiteWhale : Module {
     enum ParamIds {
         CLOCK_PARAM,
         PARAM_PARAM,
-		BUTTON_PARAM,
+        BUTTON_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
@@ -139,14 +139,14 @@ struct WhiteWhale : Module {
 
     json_t *toJson() override {
         json_t *rootJ = json_object();
-		json_object_set_new(rootJ, "current", json_string(base64_encode((unsigned char*)&w, sizeof(whale_set)).c_str()));
-		json_object_set_new(rootJ, "flash", json_string(base64_encode((unsigned char*)&flashy, sizeof(nvram_data_t)).c_str()));
+        json_object_set_new(rootJ, "current", json_string(base64_encode((unsigned char*)&w, sizeof(whale_set)).c_str()));
+        json_object_set_new(rootJ, "flash", json_string(base64_encode((unsigned char*)&flashy, sizeof(nvram_data_t)).c_str()));
         return rootJ;
     }
 
     void fromJson(json_t *rootJ) override {
-		memcpy((void*)&w, base64_decode(json_string_value(json_object_get(rootJ, "current"))).c_str(), sizeof(whale_set));
-		memcpy((void*)&flashy, base64_decode(json_string_value(json_object_get(rootJ, "flash"))).c_str(), sizeof(nvram_data_t));
+        memcpy((void*)&w, base64_decode(json_string_value(json_object_get(rootJ, "current"))).c_str(), sizeof(whale_set));
+        memcpy((void*)&flashy, base64_decode(json_string_value(json_object_get(rootJ, "flash"))).c_str(), sizeof(nvram_data_t));
     }
 
     void reset() override {
@@ -172,12 +172,12 @@ void WhiteWhale::step() {
         vgpio_set(B08, externalClock);
         simulate_external_clock_interrupt();
     }
-	bool frontButton = params[BUTTON_PARAM].value == 0;
-	if (frontButton != vgpio_get(NMI))
-	{
-		vgpio_set(NMI, frontButton);
-		simulate_front_button_interrupt();
-	}
+    bool frontButton = params[BUTTON_PARAM].value == 0;
+    if (frontButton != vgpio_get(NMI))
+    {
+        vgpio_set(NMI, frontButton);
+        simulate_front_button_interrupt();
+    }
 
     // Convert knob float parameters to 12-bit ADC values
     vadc_set(0, params[CLOCK_PARAM].value * 0xFFF);
@@ -230,7 +230,7 @@ WhiteWhaleWidget::WhiteWhaleWidget() {
     addParam(createParam<RoundSmallBlackKnob>(Vec(12, 30), module, WhiteWhale::PARAM_PARAM, 0.0, 1.0, 0.5));
     addParam(createParam<RoundSmallBlackKnob>(Vec(12, 250), module, WhiteWhale::CLOCK_PARAM, 0.0, 1.0, 0.5));
 
-	addParam(createParam<TL1105>(Vec(62, 337), module, WhiteWhale::BUTTON_PARAM, 0.0, 1.0, 0.0));
+    addParam(createParam<TL1105>(Vec(62, 337), module, WhiteWhale::BUTTON_PARAM, 0.0, 1.0, 0.0));
 
     addChild(createLight<MediumLight<YellowLight>>(Vec(77, 286), module, WhiteWhale::CLOCK_LIGHT));
     addChild(createLight<MediumLight<YellowLight>>(Vec(77, 84), module, WhiteWhale::TRIG1_LIGHT));
@@ -252,6 +252,6 @@ WhiteWhaleWidget::WhiteWhaleWidget() {
 
 void WhiteWhaleWidget::randomize()
 {
-	// don't randomize params, just randomize module core
-	module->randomize();
+    // don't randomize params, just randomize module core
+    module->randomize();
 }
