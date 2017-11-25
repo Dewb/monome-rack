1b7c9e77376a2578f607c35d39e9233ff85a9d8d
Michael Dewberry <michael.dewberry@gmail>
2017-11-24 22:56:21 -0500

    UX for held grid keys

--- grid.cpp
+++ grid.cpp
@@ -36,10 +36,10 @@ struct Grid : Module {
             for (size_t j = 0; j < GRID_Y; j++)
             {
                 int n = i | (j << 4);
-                if (params[n].value != pressedState[n])
+                if ((params[n].value > 0) != pressedState[n])
                 {
-                    simulate_monome_key(i, j, params[n].value);
-                    pressedState[n] = params[n].value;
+                    simulate_monome_key(i, j, params[n].value > 0 ? 1 : 0);
+                    pressedState[n] = params[n].value > 0;
                 }
             }
         }
@@ -61,13 +61,19 @@ struct Grid : Module {
 };
 
 
-struct MonomePad : ParamWidget {
+struct MonomeKey : ParamWidget {
 
     uint8_t* ledAddress;
     int key_x;
     int key_y;
 
-    MonomePad() : ParamWidget() {
+    typedef enum {
+        OFF,
+        PRESSED,
+        HELD
+    } KeyPressState;
+
+    MonomeKey() : ParamWidget() {
     }
 
     Grid128Widget* getGrid() {
@@ -87,6 +93,14 @@ struct MonomePad : ParamWidget {
         NVGcolor color1 = nvgRGB(val * 14 + 31, val * 13 + 31, val * 11 + 16);
         NVGcolor color2 = nvgRGB(val * 12 + 31, val * 11 + 15, val * 10 + 8);
 
+        if (value == HELD) 
+        {
+            nvgBeginPath(vg);
+            nvgRoundedRect(vg, -3, -3, box.size.x + 6, box.size.y + 6, 6);
+            nvgFillColor(vg, nvgRGB(180, 180, 0));
+            nvgFill(vg);
+        }
+
         nvgBeginPath(vg);
         auto paint = nvgBoxGradient(vg, 0, 0, box.size.x, box.size.y, box.size.x * 0.5, box.size.x * 1.2, color1, color2);
         nvgRoundedRect(vg, 0, 0, box.size.x, box.size.y, 4);
@@ -101,30 +115,54 @@ struct MonomePad : ParamWidget {
         nvgFill(vg);
     }
 
+    void beginPress()
+    {
+        if (guiIsModPressed() && value != HELD) {
+            // hold key down
+            setValue(HELD);
+        } else {
+            setValue(PRESSED);
+        }
+    }
+
+    void endPress()
+    {
+        if (value != HELD)
+        {
+            setValue(OFF);
+        }
+    }
+
     void onMouseDown(EventMouseDown &e) override
     {
+        if (e.button == 1) { 
+            return; 
+        }
         e.target = this;
         e.consumed = true;
-        setValue(1.0);
+        beginPress();
     }
 
     void onMouseUp(EventMouseUp &e) override
     {
+        if (e.button == 1) { 
+            return; 
+        }
         e.target = this;
         e.consumed = true;
-        setValue(0.0);
+        endPress();
     }
 
     void onDragStart(EventDragStart &e) override
     {
-        getGrid()->padsTouchedThisDrag.clear();
-        getGrid()->padsTouchedThisDrag.insert(this);
+        getGrid()->keysTouchedThisDrag.clear();
+        getGrid()->keysTouchedThisDrag.insert(this);
     }
 
     void onDragEnd(EventDragEnd &e) override
     {
-        setValue(0.0);
-        getGrid()->padsTouchedThisDrag.clear();
+        endPress();
+        getGrid()->keysTouchedThisDrag.clear();
     }
 
     void onDragMove(EventDragMove &e) override
@@ -133,16 +171,16 @@ struct MonomePad : ParamWidget {
 
     void onDragLeave(EventDragEnter &e) override
     {
-        setValue(0.0);
+        endPress();
     }
 
     void onDragEnter(EventDragEnter &e) override
     {
-        auto ret = getGrid()->padsTouchedThisDrag.insert(this);
+        auto ret = getGrid()->keysTouchedThisDrag.insert(this);
         if (ret.second) 
         {
             // this button wasn't already in the touched set
-            setValue(1.0);
+            beginPress();
         }
     }
 };
@@ -177,32 +215,110 @@ Grid128Widget::Grid128Widget() {
             int y = margins.y + j * (button_size + spacing);
             int n = i | (j << 4);
 
-            MonomePad* pad = (MonomePad*)createParam<MonomePad>(Vec(x, y), module, n, 0, 1.0, 0);
-            pad->setKeyAddress(x, y, monomeLedBuffer + n);
-            pad->box.size = Vec(button_size, button_size);
-            addParam(pad);
+            MonomeKey* key = (MonomeKey*)createParam<MonomeKey>(Vec(x, y), module, n, 0, 2.0, 0);
+            key->setKeyAddress(x, y, monomeLedBuffer + n);
+            key->box.size = Vec(button_size, button_size);
+            addParam(key);
         }
     }
 }
 
 json_t *Grid128Widget::toJson() {
-	json_t *rootJ = json_object();
+    json_t *rootJ = json_object();
 
-	// manufacturer
-	json_object_set_new(rootJ, "plugin", json_string(model->plugin->slug.c_str()));
-	// model
-	json_object_set_new(rootJ, "model", json_string(model->slug.c_str()));
-	// pos
-	json_t *posJ = json_pack("[f, f]", (double) box.pos.x, (double) box.pos.y);
-	json_object_set_new(rootJ, "pos", posJ);
-	
-	return rootJ;
+    // manufacturer
+    json_object_set_new(rootJ, "plugin", json_string(model->plugin->slug.c_str()));
+    // model
+    json_object_set_new(rootJ, "model", json_string(model->slug.c_str()));
+    // pos
+    json_t *posJ = json_pack("[f, f]", (double) box.pos.x, (double) box.pos.y);
+    json_object_set_new(rootJ, "pos", posJ);
+    
+    return rootJ;
 }
 
 void Grid128Widget::fromJson(json_t *rootJ) {
-	// pos
-	json_t *posJ = json_object_get(rootJ, "pos");
-	double x, y;
-	json_unpack(posJ, "[F, F]", &x, &y);
-	box.pos = Vec(x, y);
+    // pos
+    json_t *posJ = json_object_get(rootJ, "pos");
+    double x, y;
+    json_unpack(posJ, "[F, F]", &x, &y);
+    box.pos = Vec(x, y);
+}
+
+void Grid128Widget::clearHeldKeys() {
+    for(auto p : params) {
+        p->setValue(MonomeKey::OFF);
+    }
+}
+
+void Grid128Widget::onMouseDown(EventMouseDown &e)
+{
+    Widget::onMouseDown(e);
+    if (e.consumed)
+        return;
+
+    if (e.button == 1) {
+        createContextMenu();
+        e.consumed = true;
+        e.target = this;
+    }
+
+    if (e.button == 0 && guiIsModPressed()) { 
+        clearHeldKeys();
+        e.target = this;
+        e.consumed = true;
+    }
+}
+
+struct CloneMenuItem : MenuItem {
+    ModuleWidget *moduleWidget;
+    void onAction(EventAction &e) override {
+        gRackWidget->cloneModule(moduleWidget);
+    }
+};
+
+struct DeleteMenuItem : MenuItem {
+    ModuleWidget *moduleWidget;
+    void onAction(EventAction &e) override {
+        gRackWidget->deleteModule(moduleWidget);
+        moduleWidget->finalizeEvents();
+        delete moduleWidget;
+    }
+};
+
+struct GridReleaseHeldKeysItem : MenuItem {
+    Grid128Widget* grid;
+    void onAction(EventAction &e) override {
+        grid->clearHeldKeys();
+    }
+};
+
+Menu *Grid128Widget::createContextMenu() {
+    Menu *menu = gScene->createMenu();
+
+    MenuLabel *menuLabel = new MenuLabel();
+    menuLabel->text = model->manufacturer + " " + model->name;
+    menu->addChild(menuLabel);
+
+    auto *cloneItem = new CloneMenuItem();
+    cloneItem->text = "Duplicate";
+    cloneItem->rightText = GUI_MOD_KEY_NAME "+D";
+    cloneItem->moduleWidget = this;
+    menu->addChild(cloneItem);
+
+    auto *deleteItem = new DeleteMenuItem();
+    deleteItem->text = "Delete";
+    deleteItem->rightText = "Backspace/Delete";
+    deleteItem->moduleWidget = this;
+    menu->addChild(deleteItem);
+
+    menu->addChild(construct<MenuEntry>());
+    menu->addChild(construct<MenuLabel>(&MenuLabel::text, "Actions"));
+    auto *releaseItem = new GridReleaseHeldKeysItem();
+    releaseItem->text = "Release held keys";
+    releaseItem->rightText = GUI_MOD_KEY_NAME "+Click";
+    releaseItem->grid = this;
+    menu->addChild(releaseItem);
+
+    return menu;
 }
--- grid.hpp
+++ grid.hpp
@@ -7,15 +7,21 @@ using namespace rack;
 // module widgets
 ////////////////////
 
-struct MonomePad;
+struct MonomeKey;
 
 struct Grid128Widget : ModuleWidget {
     Grid128Widget();
+
     json_t* toJson() override;
     void fromJson(json_t *rootJ) override;
+    Menu *createContextMenu() override;
+    void onMouseDown(EventMouseDown &e) override;
+
+    void clearHeldKeys();
+
 protected:
-    friend struct MonomePad;
-    std::unordered_set<MonomePad*> padsTouchedThisDrag;
+    friend struct MonomeKey;
+    std::unordered_set<MonomeKey*> keysTouchedThisDrag;
 };
 
 
