25552c79b72626b0c840e591b4628df74e2f8bbf
Michael Dewberry <michael.dewberry@gmail>
2019-02-12 01:07:08 -0500

    Improve curl script

--- .github/actions/tag_latest/entrypoint.sh
+++ .github/actions/tag_latest/entrypoint.sh
@@ -2,20 +2,21 @@
 
 set -eu
 
-GITHUB_API_URL=https://api.github.com/
+GITHUB_API_URL=https://api.github.com
+REPO=${GITHUB_REPOSITORY#"https://github.com/"} # work around url parsing bug in act for local testing
 
 # If the action was triggered on a branch other than master, terminate with neutral status
-if ["${GITHUB_REF}" != "refs/heads/master"]; then
+if [ ${GITHUB_REF} != "refs/heads/master" ] ; then
     exit 78
 fi
 
 # Update the "latest" tag
 TAG=latest
 REF=refs/heads/master
-curl \
+curl -v \
     --header "Authorization: token ${GITHUB_TOKEN}" \
     --header "Content-Type: application/json" \
     --request PATCH \
-    --data '{ "force":"false", "sha":"${GITHUB_SHA}" }' \
-    ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/git/refs/${TAG}
+    --data '{ "sha":"${GITHUB_SHA}" }' \
+    ${GITHUB_API_URL}/repos/${REPO}/git/refs/${TAG}
 
