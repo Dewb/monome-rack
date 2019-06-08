73680d021c89c6cea1f0c0b783148689af627460
Michael Dewberry <michael.dewberry@gmail>
2019-06-08 18:53:28 -0400

    osxcross build compiles, not running yet

--- .github/actions/build_osx/entrypoint.sh
+++ .github/actions/build_osx/entrypoint.sh
@@ -4,7 +4,10 @@ set -eu
 
 export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK
 export RACK_USER_DIR=${GITHUB_WORKSPACE}
-export CROSS_TRIPLE=x86_64-apple-darwin
 
-/usr/bin/crossbuild make clean
-/usr/bin/crossbuild make install
+export CC=x86_64-apple-darwin15-clang
+export CXX=x86_64-apple-darwin15-clang++
+export STRIP=x86_64-apple-darwin15-strip
+
+make clean
+make install
