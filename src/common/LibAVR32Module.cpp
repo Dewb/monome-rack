Dewb/monome-rack

commit 6d6ee7ebc3eb740723a6aaef7207b59a9c973828

Simplify teletype screen drawing logic

src/common/LibAVR32Module.hpp
@@ -64,11 +64,11 @@ struct LibAVR32Module : rack::engine::Module, GridConsumer
     int outputRate;
 
     std::mutex processMutex;
+    bool firstStep;
 
 private:
     void reloadFirmware(bool preserveMemory);
 
-    bool firstStep;
     ReloadRequest reloadRequested;
 
     float dacOffsetVolts;

src/teletype/TeletypeScreenWidget.cpp
@@ -1,10 +1,8 @@
 #include "TeletypeScreenWidget.hpp"
 #include "TeletypeModule.hpp"
 
-TeletypeScreenWidget::TeletypeScreenWidget(uint8_t* buffer, int x, int y)
-    : buffer(buffer)
-    , pixel_x(x)
-    , pixel_y(y)
+TeletypeScreenWidget::TeletypeScreenWidget(LibAVR32Module* module)
+: module(module)
 {
 }
 
@@ -177,24 +175,19 @@ void TeletypeScreenWidget::onSelectKey(const event::SelectKey& e)
         }
     }
 
-    auto mw = static_cast<ModuleWidget*>(parent);
-    if (mw)
+    if (module)
     {
-        auto tt = static_cast<TeletypeModule*>(mw->module);
-        if (tt)
+        if (e.action == GLFW_PRESS)
         {
-            if (e.action == GLFW_PRESS)
-            {
-                tt->firmware.hidMessage(key, mod, false, false);
-            }
-            else if (e.action == GLFW_REPEAT)
-            {
-                tt->firmware.hidMessage(key, mod, true, false);
-            }
-            else if (e.action == GLFW_RELEASE)
-            {
-                tt->firmware.hidMessage(key, mod, false, true);
-            }
+            module->firmware.hidMessage(key, mod, false, false);
+        }
+        else if (e.action == GLFW_REPEAT)
+        {
+            module->firmware.hidMessage(key, mod, true, false);
+        }
+        else if (e.action == GLFW_RELEASE)
+        {
+            module->firmware.hidMessage(key, mod, false, true);
         }
     }
 
@@ -203,72 +196,86 @@ void TeletypeScreenWidget::onSelectKey(const event::SelectKey& e)
 
 void TeletypeScreenWidget::draw(const DrawArgs& args)
 {
-    drawLayer(args, 0);
+    drawFrame(args.vg);
 }
 
 void TeletypeScreenWidget::drawLayer(const DrawArgs& args, int layer)
 {
-    auto vg = args.vg;
-    
-    int margin = 5;
-
-    float pixel_width = (box.size.x - 2 * margin) / (pixel_x * 1.0);
-    float pixel_height = (box.size.y - 2 * margin) / (pixel_y * 1.0);
-
-    if (layer == 0)
+    if (layer == 1)
     {
-        if (this == APP->event->selectedWidget)
-        {
-            // draw keyboard focus highlight rectangle
-            nvgBeginPath(vg);
-            nvgRoundedRect(vg, -2, -2, box.size.x + 4, box.size.y + 4, 2.5);
-            nvgStrokeColor(vg, nvgRGB(190, 180, 0));
-            nvgStrokeWidth(vg, 3.5);
-            nvgStroke(vg);
-            nvgFillColor(vg, nvgRGB(190, 180, 0));
-            nvgFill(vg);
-        } else {
-            // draw skeumorphic shadow around screen
-            float t = 0.95;
-            float r = 2.75;
-            nvgBeginPath(vg);
-            nvgRoundedRect(vg, box.size.x - t, -t, 2 * t, box.size.y + t, r);
-            nvgFillColor(vg, nvgRGB(250, 250, 240));
-            nvgFill(vg);
+        drawPixels(args.vg);
+    }
+}
 
-            nvgBeginPath(vg);
-            nvgRoundedRect(vg, -t, -t, box.size.x + 2 * t, 2 * t, r);
-            nvgRoundedRect(vg, -t, -t, 2 * t, box.size.y + t, r);
-            nvgFillColor(vg, nvgRGB(140, 140, 130));
-            nvgFill(vg);
-
-            nvgBeginPath(vg);
-            nvgRoundedRect(vg, -t, box.size.y - t, box.size.x + 2 * t, 2 * t, r);
-            nvgFillColor(vg, nvgRGB(250, 250, 240));
-            nvgFill(vg);
-        }
-
-        // the screen itself
+void TeletypeScreenWidget::drawFrame(NVGcontext* vg)
+{
+    if (this == APP->event->selectedWidget)
+    {
+        // draw keyboard focus highlight rectangle
         nvgBeginPath(vg);
-        nvgRoundedRect(vg, 0, 0, box.size.x, box.size.y, 2);
-        nvgFillColor(vg, nvgRGB(0, 0, 0));
+        nvgRoundedRect(vg, -2, -2, box.size.x + 4, box.size.y + 4, 2.5);
+        nvgStrokeColor(vg, nvgRGB(190, 180, 0));
+        nvgStrokeWidth(vg, 3.5);
+        nvgStroke(vg);
+        nvgFillColor(vg, nvgRGB(190, 180, 0));
         nvgFill(vg);
     }
-    else if (layer == 1)
+    else
     {
-        // now draw the pixels
-        if (buffer)
-        {
-            uint8_t* ptr = buffer;
-            for (int j = 0; j < pixel_y; j++)
-            {
-                for (int i = 0; i < pixel_x; i++)
-                {
-                    float x = margin + i * pixel_width;
-                    float y = margin + j * pixel_height;
+        // draw skeumorphic shadow around screen
+        float t = 0.95;
+        float r = 2.75;
+        nvgBeginPath(vg);
+        nvgRoundedRect(vg, box.size.x - t, -t, 2 * t, box.size.y + t, r);
+        nvgFillColor(vg, nvgRGB(250, 250, 240));
+        nvgFill(vg);
 
-                    drawPixel(vg, x, y, pixel_width, pixel_height, *ptr++);
-                }
+        nvgBeginPath(vg);
+        nvgRoundedRect(vg, -t, -t, box.size.x + 2 * t, 2 * t, r);
+        nvgRoundedRect(vg, -t, -t, 2 * t, box.size.y + t, r);
+        nvgFillColor(vg, nvgRGB(140, 140, 130));
+        nvgFill(vg);
+
+        nvgBeginPath(vg);
+        nvgRoundedRect(vg, -t, box.size.y - t, box.size.x + 2 * t, 2 * t, r);
+        nvgFillColor(vg, nvgRGB(250, 250, 240));
+        nvgFill(vg);
+    }
+
+    // draw the empty screen
+    nvgBeginPath(vg);
+    nvgRoundedRect(vg, 0, 0, box.size.x, box.size.y, 2);
+    nvgFillColor(vg, nvgRGB(0, 0, 0));
+    nvgFill(vg);
+}
+
+void TeletypeScreenWidget::drawPixels(NVGcontext* vg)
+{
+    if (!module || module->firstStep)
+    {
+        return;
+    }
+
+    uint8_t* buffer;
+    uint16_t pixel_x = 0;
+    uint16_t pixel_y = 0;
+
+    module->firmware.getScreenBuffer(&buffer, &pixel_x, &pixel_y);
+
+    if (buffer)
+    {
+        int margin = 5;
+        float pixel_width = (box.size.x - 2 * margin) / (pixel_x * 1.0);
+        float pixel_height = (box.size.y - 2 * margin) / (pixel_y * 1.0);
+
+        for (int j = 0; j < pixel_y; j++)
+        {
+            for (int i = 0; i < pixel_x; i++)
+            {
+                float x = margin + i * pixel_width;
+                float y = margin + j * pixel_height;
+
+                drawPixel(vg, x, y, pixel_width, pixel_height, *buffer++);
             }
         }
     }

firmware/mock_hardware/mock_hardware_api.c
@@ -229,20 +229,20 @@ void hardware_getScreenBuffer(uint8_t** ptr, uint16_t* width, uint16_t* height)
         memset(screenBuffer, 0, sizeof(uint8_t) * SCREEN_WIDTH * SCREEN_HEIGHT);
 
         // initialize a distinctive pattern for debugging purposes
-        for (int j = 0; j < SCREEN_HEIGHT; j++)
-        {
-            for (int i = 0; i < SCREEN_WIDTH; i++)
-            {
-                if ((j / 16) % 2)
-                {
-                    screenBuffer[i + SCREEN_WIDTH * j] = i % 16;
-                }
-                else
-                {
-                    screenBuffer[i + SCREEN_WIDTH * j] = 15 - (i % 16);
-                }
-            }
-        }
+        // for (int j = 0; j < SCREEN_HEIGHT; j++)
+        // {
+        //     for (int i = 0; i < SCREEN_WIDTH; i++)
+        //     {
+        //         if ((j / 16) % 2)
+        //         {
+        //             screenBuffer[i + SCREEN_WIDTH * j] = i % 16;
+        //         }
+        //         else
+        //         {
+        //             screenBuffer[i + SCREEN_WIDTH * j] = 15 - (i % 16);
+        //         }
+        //     }
+        // }
     }
     if (ptr)
     {
@@ -258,13 +258,6 @@ void hardware_getScreenBuffer(uint8_t** ptr, uint16_t* width, uint16_t* height)
     }
 }
 
-void hardware_copyScreenBuffer(uint8_t* dest)
-{
-    if (screenBuffer && dest)
-    {
-        memcpy(dest, screenBuffer, SCREEN_WIDTH * SCREEN_HEIGHT);
-    }
-}
 
 // TODO: make this generic for all followers somehow
 uint16_t faderbank[16];

firmware/mock_hardware/mock_hardware_api.h
@@ -36,7 +36,6 @@ MOCK_API(void, readVRAM, (void** ptr, uint32_t* size));
 MOCK_API(void, writeVRAM, (const void* src, uint32_t size));
 
 MOCK_API(void, getScreenBuffer, (uint8_t * *ptr, uint16_t* width, uint16_t* height));
-MOCK_API(void, copyScreenBuffer, (uint8_t* dest));
 
 MOCK_API(void, hidConnect, ());
 MOCK_API(void, hidDisconnect, ());

src/common/FirmwareManager.cpp
@@ -410,14 +410,6 @@ void FirmwareManager::getScreenBuffer(uint8_t** ptr, uint16_t* width, uint16_t* 
     }
 }
 
-void FirmwareManager::copyScreenBuffer(uint8_t* dest)
-{
-    if (impl)
-    {
-        impl->fw_fn_hardware_copyScreenBuffer(dest);
-    }
-}
-
 void FirmwareManager::hidConnect()
 {
     if (impl)

src/teletype/TeletypeModule.cpp
@@ -128,6 +128,4 @@ void TeletypeModule::processOutputs(const ProcessArgs& args)
     outputs[CV2_OUTPUT].setVoltage(cv2);
     outputs[CV3_OUTPUT].setVoltage(cv3);
     outputs[CV4_OUTPUT].setVoltage(cv4);
-
-    firmware.copyScreenBuffer(screenBuffer);
 }

src/teletype/TeletypeModule.hpp
@@ -51,10 +51,9 @@ struct TeletypeModule : LibAVR32Module
     };
 
     TeletypeModule();
+
     void processInputs(const ProcessArgs& args) override;
     void processOutputs(const ProcessArgs& args) override;
-
-    uint8_t screenBuffer[128 * 64];
 
 protected:
     iiDevice _iiDevice;

src/teletype/TeletypeScreenWidget.hpp
@@ -1,14 +1,11 @@
 #include "rack.hpp"
+#include "LibAVR32Module.hpp"
 
 using namespace rack;
 
 struct TeletypeScreenWidget : OpaqueWidget
 {
-    uint8_t* buffer;
-    int pixel_x;
-    int pixel_y;
-
-    TeletypeScreenWidget(uint8_t* buffer, int x, int y);
+    TeletypeScreenWidget(LibAVR32Module* module);
 
     virtual void onSelect(const event::Select& e) override;
     virtual void onDeselect(const event::Deselect& e) override;
@@ -16,5 +13,10 @@ struct TeletypeScreenWidget : OpaqueWidget
     virtual void draw(const DrawArgs& args) override;
     virtual void drawLayer(const DrawArgs& args, int layer) override;
 
+protected:
+    void drawFrame(NVGcontext* vg);
+    void drawPixels(NVGcontext* vg);
     void drawPixel(NVGcontext* vg, float x, float y, float width, float height, int data);
+
+    LibAVR32Module* module;
 };

commit 62134393002fd67708d25ef28d9ac566638a4b2c

Fix crash in firmware reload introduced in 6d6ee7ebc3eb7

src/common/LibAVR32Module.cpp
@@ -179,6 +179,8 @@ void LibAVR32Module::readSerialMessages()
 
 void LibAVR32Module::reloadFirmware(bool preserveMemory)
 {
+    std::lock_guard<std::mutex> lock(firmwareMutex);
+
     void *data, *nvram_copy, *vram_copy = 0;
     uint32_t nvram_size, vram_size = 0;
 

src/common/LibAVR32Module.hpp
@@ -64,6 +64,8 @@ struct LibAVR32Module : rack::engine::Module, GridConsumer
     int outputRate;
 
     std::mutex processMutex;
+    std::mutex firmwareMutex;
+
     bool firstStep;
 
 private:

src/teletype/TeletypeScreenWidget.cpp
@@ -245,6 +245,8 @@ void TeletypeScreenWidget::drawPixels(NVGcontext* vg)
     uint16_t pixel_x = 0;
     uint16_t pixel_y = 0;
 
+    std::lock_guard<std::mutex> lock(module->firmwareMutex);
+
     module->firmware.getScreenBuffer(&buffer, &pixel_x, &pixel_y);
 
     if (buffer)