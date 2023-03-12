Dewb/monome-rack

commit 53010d7c6d11294311cd58c8ad525973a419c56c

Support LED themes on teletype screen

src/teletype/TeletypeModule.cpp
@@ -135,9 +135,22 @@ void TeletypeModule::processOutputs(const ProcessArgs& args)
     outputs[CV4_OUTPUT].setVoltage(cv4);
 }
 
+json_t* TeletypeModule::dataToJson()
+{
+    json_t* rootJ = json_object();
+    json_object_set_new(rootJ, "theme", json_integer(theme));
+    return rootJ;
+}
+
 void TeletypeModule::dataFromJson(json_t* rootJ)
 {
     LibAVR32Module::dataFromJson(rootJ);
+
+    auto json_theme = json_object_get(rootJ, "theme");
+    if (json_theme)
+    {
+        theme = static_cast<GridTheme>(json_integer_value(json_theme));
+    }
 
     // Special json key only in factory presets, not saved in dataToJson
     json_t* jd = json_object_get(rootJ, "loadScript");

src/common/LibAVR32Module.hpp
@@ -1,5 +1,6 @@
 #include "FirmwareManager.hpp"
 #include "GridConnection.hpp"
+#include "VirtualGridTheme.hpp"
 #include "rack.hpp"
 #include <queue>
 
@@ -70,6 +71,8 @@ struct LibAVR32Module : rack::engine::Module, GridConsumer
 
     int inputRate;
     int outputRate;
+
+    GridTheme theme;
 
     virtual uint8_t* getScreenBuffer() { return 0; }
 

src/teletype/TeletypeModule.hpp
@@ -56,6 +56,7 @@ struct TeletypeModule : LibAVR32Module
     void processInputs(const ProcessArgs& args) override;
     void processOutputs(const ProcessArgs& args) override;
 
+    json_t* dataToJson() override;
     void dataFromJson(json_t* rootJ) override;
 
     rack::dsp::SchmittTrigger inputTriggers[8];

src/teletype/TeletypeWidget.cpp
@@ -111,6 +111,10 @@ void TeletypeWidget::appendContextMenu(rack::Menu* menu)
 
     menu->addChild(new MenuSeparator());
 
+    menu->addChild(createIndexPtrSubmenuItem("Theme", { "Red", "Orange", "Yellow", "White" }, &m->theme));
+
+    menu->addChild(new MenuSeparator());
+
     menu->addChild(new InternalPresetSubmenu(m, "Import scenes", Load));
     menu->addChild(new InternalPresetSubmenu(m, "Export scenes", Save));
 

src/teletype/screen/TeletypeScreenWidget.cpp
@@ -205,12 +205,14 @@ void TeletypeScreenWidget::drawFrame(NVGcontext* vg)
     if (this == APP->event->selectedWidget)
     {
         // draw keyboard focus highlight rectangle
+        NVGcolor color;
+        levelToGradient(module->theme, 14, &color, nullptr);
         nvgBeginPath(vg);
         nvgRoundedRect(vg, -2, -2, box.size.x + 4, box.size.y + 4, 2.5);
-        nvgStrokeColor(vg, nvgRGB(190, 180, 0));
+        nvgStrokeColor(vg, color);
         nvgStrokeWidth(vg, 3.5);
         nvgStroke(vg);
-        nvgFillColor(vg, nvgRGB(190, 180, 0));
+        nvgFillColor(vg, color);
         nvgFill(vg);
     }
     else
@@ -245,6 +247,7 @@ void TeletypeScreenWidget::drawFrame(NVGcontext* vg)
 void TeletypeScreenWidget::drawPixels(NVGcontext* vg)
 {
     uint8_t* buffer = module ? module->getScreenBuffer() : exampleScreen;
+    GridTheme theme = module ? module->theme : GridTheme::Yellow;
     uint16_t pixel_x = 128;
     uint16_t pixel_y = 64;
 
@@ -274,15 +277,21 @@ void TeletypeScreenWidget::drawPixels(NVGcontext* vg)
             float x = margin + i * pixel_width;
             float y = margin + j * pixel_height;
 
-            drawPixel(vg, x, y, pixel_width, pixel_height, *buffer++);
+            uint8_t d = *buffer++;
+            if (d > 0)
+            {
+                drawPixel(vg, theme, x, y, pixel_width, pixel_height, d);
+            }
         }
     }
 }
 
-void TeletypeScreenWidget::drawPixel(NVGcontext* vg, float x, float y, float width, float height, int data)
+void TeletypeScreenWidget::drawPixel(NVGcontext* vg, GridTheme theme, float x, float y, float width, float height, int data)
 {
     nvgBeginPath(vg);
     nvgRect(vg, x, y, width * 0.92, height * 0.92);
-    nvgFillColor(vg, nvgRGB(data ? data * 11 + 79 : 0, data ? data * 11 + 79 : 0, data * 11));
+    NVGcolor color;
+    levelToGradient(theme, data, &color, nullptr);
+    nvgFillColor(vg, color);
     nvgFill(vg);
 }

src/teletype/screen/TeletypeScreenWidget.hpp
@@ -1,5 +1,6 @@
-#include "rack.hpp"
 #include "LibAVR32Module.hpp"
+#include "VirtualGridTheme.hpp"
+#include "rack.hpp"
 
 using namespace rack;
 
@@ -16,7 +17,7 @@ struct TeletypeScreenWidget : OpaqueWidget
 protected:
     void drawFrame(NVGcontext* vg);
     void drawPixels(NVGcontext* vg);
-    void drawPixel(NVGcontext* vg, float x, float y, float width, float height, int data);
+    void drawPixel(NVGcontext* vg, GridTheme theme, float x, float y, float width, float height, int data);
 
     LibAVR32Module* module;
 };

commit bbbf6b6718da1a1db00b8175eb6c0cb1b77b277b

Fix teletype state saving, broken in 53010d7

src/teletype/TeletypeModule.cpp
@@ -137,8 +137,10 @@ void TeletypeModule::processOutputs(const ProcessArgs& args)
 
 json_t* TeletypeModule::dataToJson()
 {
-    json_t* rootJ = json_object();
+    json_t* rootJ = LibAVR32Module::dataToJson();
+
     json_object_set_new(rootJ, "theme", json_integer(theme));
+
     return rootJ;
 }
 