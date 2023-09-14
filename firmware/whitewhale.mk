03f7ae8cef9540f0d2cf4252510dbe1e0e34b6a8
Michael Dewberry <michael.dewberry@gmail>
2023-09-13 22:48:00 -0400

    Add white whale kria alt firmware

--- firmware/whitewhale.mk
+++ firmware/whitewhale.mk
@@ -1,27 +1,25 @@
-TARGET_NAME := whitewhale
-
 FLAGS += \
 	-D__AVR32_UC3B0256__ \
 	-Imock_hardware \
 	-Imock_hardware/include \
-	-Iwhitewhale/libavr32/src \
-	-Iwhitewhale/libavr32/src/usb/midi \
-	-Iwhitewhale/libavr32/src/usb/hid \
-	-Iwhitewhale/libavr32/src/usb/cdc \
-	-Iwhitewhale/libavr32/asf/common/services/usb \
-	-Iwhitewhale/libavr32/asf/common/services/usb/uhc \
-	-Iwhitewhale/libavr32/conf \
-	-Iwhitewhale/libavr32/conf/trilogy \
+	-I$(TARGET_NAME)/libavr32/src \
+	-I$(TARGET_NAME)/libavr32/src/usb/midi \
+	-I$(TARGET_NAME)/libavr32/src/usb/hid \
+	-I$(TARGET_NAME)/libavr32/src/usb/cdc \
+	-I$(TARGET_NAME)/libavr32/asf/common/services/usb \
+	-I$(TARGET_NAME)/libavr32/asf/common/services/usb/uhc \
+	-I$(TARGET_NAME)/libavr32/conf \
+	-I$(TARGET_NAME)/libavr32/conf/trilogy \
 	
 SOURCES = \
-	whitewhale/src/main.c \
-	whitewhale/libavr32/src/events.c \
-	whitewhale/libavr32/src/timers.c \
-	whitewhale/libavr32/src/util.c \
+	$(TARGET_NAME)/src/main.c \
+	$(TARGET_NAME)/libavr32/src/events.c \
+	$(TARGET_NAME)/libavr32/src/timers.c \
+	$(TARGET_NAME)/libavr32/src/util.c \
 	$(wildcard mock_hardware/*.c) \
 	$(wildcard mock_hardware/common/*.c) \
 	$(wildcard mock_hardware/modules/trilogy/*.c) \
 
-FLAGS += -DGIT_VERSION="\"$(shell cut -d '-' -f 1 <<< $(shell cd whitewhale; git describe --tags | cut -c 1-)) $(shell cd whitewhale; git describe --always --dirty --exclude '*' | tr '[a-z]' '[A-Z]')\""
+FLAGS += -DGIT_VERSION="\"$(shell cut -d '-' -f 1 <<< $(shell cd $(TARGET_NAME); git describe --tags | cut -c 1-)) $(shell cd $(TARGET_NAME); git describe --always --dirty --exclude '*' | tr '[a-z]' '[A-Z]')\""
 
 include common.mk
