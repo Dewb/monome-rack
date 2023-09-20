8c82ee7e14a8050bc19a9c5a82b0ff8b28b74f26
Michael Dewberry <michael.dewberry@gmail>
2023-09-20 03:33:28 -0400

    Support 90mm and 60mm faders on the faderbank

--- src/faderbank/FaderbankModule.cpp
+++ src/faderbank/FaderbankModule.cpp
@@ -18,7 +18,7 @@ FaderbankModule::FaderbankModule()
 
     for (unsigned i = 0; i < NUM_FADERS; i++)
     {
-        configParam<FBFaderParam>(i, 0.0, 10.0, i / (1.0 * NUM_FADERS) * 10.0);
+        configParam<FBFaderParam>(i, 0.0, 10.0, 0.0);
     }
 
     _iiDevice.setAddress(0x34);
@@ -70,6 +70,7 @@ json_t* FaderbankModule::dataToJson()
 {
     json_t* rootJ = json_object();
     json_object_set_new(rootJ, "midi", midiInput.toJson());
+    json_object_set_new(rootJ, "faderSize", json_integer(faderSize));
     return rootJ;
 }
 
@@ -78,4 +79,8 @@ void FaderbankModule::dataFromJson(json_t* rootJ)
     json_t* midiJ = json_object_get(rootJ, "midi");
     if (midiJ)
         midiInput.fromJson(midiJ);
+
+    json_t* faderSizeJ = json_object_get(rootJ, "faderSize");
+    if (faderSizeJ)
+        faderSize = static_cast<FaderSize>(json_integer_value(faderSizeJ));
 }
