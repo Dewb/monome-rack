f5d268f38dde3e68c9b03f669c8fe3f4e56030b4
Michael Dewberry <michael.dewberry@gmail>
2024-10-30 17:44:31 -0400

    Resolve warning to fix building in gcc 13

--- firmware/mock_hardware/common/adc.c
+++ firmware/mock_hardware/common/adc.c
@@ -1,10 +1,10 @@
 #include "mock_hardware_api.h"
 #include "types.h"
 
-void adc_convert(u16 adc[4])
+void adc_convert(u16 (*adc)[4])
 {
-    adc[0] = hardware_getADC(0);
-    adc[1] = hardware_getADC(1);
-    adc[2] = hardware_getADC(2);
-    adc[3] = hardware_getADC(3);
+    (*adc)[0] = hardware_getADC(0);
+    (*adc)[1] = hardware_getADC(1);
+    (*adc)[2] = hardware_getADC(2);
+    (*adc)[3] = hardware_getADC(3);
 }
--- firmware/mock_hardware/include/adc.h
+++ firmware/mock_hardware/include/adc.h
@@ -1,4 +1,4 @@
 #include "types.h"
 
 void init_adc(void);
-void adc_convert(u16 dst[4]);
+void adc_convert(u16 (*dst)[4]);
